358658168fc971712f26f6a1f7edcb5f93e07fd4
unknown <you@example.com>
2020-05-14 15:01:17 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$('.slides1').click(function(){
+//    $('.slides1').css('background','#f7ac34');
+
+    $('header').addClass('back1');
+    
+    
+});
+$('.slides2').click(function(){
+
+    $('header').addClass('back2');
+    
+    
+});
+$('.slides3').click(function(){
+
+    $('header').addClass('back3');
+    
+    
+});
+$('.slides4').click(function(){
+
+    $('header').addClass('back4');
+    
+    
+});
+
+// $("button").click(function(){
+//        $(".modal-wrapper").fadeIn();
+//        $(".modal-content").addClass('open');
+//
+//    })
